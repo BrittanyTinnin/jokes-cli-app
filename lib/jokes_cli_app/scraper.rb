6219f19c2a536b5f54ca756d3a5e5ef98df88f2d
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-21 17:39:24 +0000

    updated scraper

--- lib/jokes_cli_app/scraper.rb
+++ lib/jokes_cli_app/scraper.rb
@@ -2,8 +2,9 @@ class Scraper
 
   def self.scrape_category
     page = Nokogiri::HTML(open("http://jokes.cc.com/"))
-    page.css("ul.list_horiz li").each do |category|
-
+    page.css("div.middle ul li").each do |category|
+      link = category.css("a").attribute("href").text
+      title = category.css("a").text
       binding.pry
     end
   end
