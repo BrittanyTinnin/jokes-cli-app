5eadc149479967be2a23ae651a320c17efd3ea0f
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-22 23:51:52 +0000

    add new file

--- lib/environment.rb
+++ lib/environment.rb
@@ -6,6 +6,7 @@ require_relative "jokes_cli_app/version"
 require_relative "jokes_cli_app/scraper"
 require_relative "jokes_cli_app/jokes"
 require_relative "jokes_cli_app/cli"
+require_relative "jokes_cli_app/new"
 
 module JokesCliApp
   # Your code goes here...
